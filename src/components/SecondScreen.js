e3cf9206b9f369ba974db5cfe9465747612193fb
Vinh <vinh230620@gmail.com>
2021-01-25 14:31:13 +0700

    25/01/2021

--- src/components/SecondScreen.js
+++ src/components/SecondScreen.js
@@ -4,34 +4,44 @@ import AppStyle from "../theme";
 import StyleFirst from "../theme/StyleFirst";
 import LinearGradient from "react-native-linear-gradient";
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { FlatList, TextInput, TouchableOpacity } from "react-native-gesture-handler";
 
-const SecondScreen = ({navigation}) => {
+const SecondScreen = ({ navigation }) => {
     const [data, setData] = useState({
-        phone: '',
-        check_textInPut: false,
+        phone : '',
     })
+    // const handlePhone = (val) => {
+    //     if (val.trim().length > 0) {
+    //         setData({
+    //             ...data,
+    //             phone: val,
+    //             check_textInPut: true,
+    //         });
+    //     }
+    //     else {
+    //         setData({
+    //             ...data,
+    //             phone: val,
+    //             check_textInPut: false,
+    //         })
+    //     }
+    // }
     const handlePhone = (val) => {
+        let clonedata = data;
         if (val.trim().length > 0) {
-            setData({
-                ...data,
-                phone: val,
-                check_textInPut: true,
-            });
+            clonedata.phone = val,
+            clonedata.isChecked = true;
         }
-        else {
-            setData({
-                ...data,
-                phone: val,
-                check_textInPut: false,
-            })
+        else{
+            clonedata.isChecked =false;
         }
+        setData({ ...clonedata })
     }
     return (
         <View style={AppStyle.StyleFirst.container}>
             <StatusBar backgroundColor='black' barStyle="light-content" />
             <View style={AppStyle.StyleSecond.content}>
-                <TouchableOpacity onPress={()=>navigation.goBack()}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Icon name="chevron-left" color='#FFFFFF' size={22} style={AppStyle.StyleSecond.icon} />
                 </TouchableOpacity>
 
@@ -39,15 +49,15 @@ const SecondScreen = ({navigation}) => {
                     <Text style={AppStyle.StyleSecond.text0}>Đăng Nhập/ Đăng Ký</Text>
                     <Text style={AppStyle.StyleSecond.text1}>Vui lòng nhập số điện thoại để tiếp tục</Text>
                 </View>
-                <TextInput 
+                <TextInput
                     style={AppStyle.StyleSecond.textinput}
                     placeholder="Nhập số điện thoại"
                     placeholderTextColor="#FFFFFF"
                     keyboardType="number-pad"
-                    
+
                     onChangeText={(val) => handlePhone(val)}
                 />
-                {data.check_textInPut ?
+                {data.isChecked ?
                     <LinearGradient
                         style={AppStyle.StyleFirst.linear}
                         colors={['#8B3BFF', '#B738FF']}
@@ -62,10 +72,17 @@ const SecondScreen = ({navigation}) => {
                         style={AppStyle.StyleFirst.linear}
                         colors={['#B738FF', '#8B3BFF']}
                     >
-                            <Text style={AppStyle.StyleSecond.text2}>Tiếp Tục</Text>
+                        <Text style={AppStyle.StyleSecond.text2}>Tiếp Tục</Text>
                     </LinearGradient>
                 }
             </View>
+
+            {/* <View>
+                {data.isChecked &&(
+                    <Text style={AppStyle.StyleFirst.text}>{data.phone}</Text>
+                ) }
+                <Text></Text>
+            </View> */}
         </View>
     )
 }
