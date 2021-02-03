a3073dcf29b19d18611e9aa37eaf946d30613108
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2021-02-03 13:17:18 +0700

    nam

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,27 +3,19 @@ import StyleSecond from './StyleSecond';
 import StyleScreenXacNhanSDT  from "./StyleScreenXacThucSDT";
 import StyleMain from "./StyleMain";
 import StyleLichSu from "./StyleLichSu";
-import StyleLichSuDiem from "./StyleLichSuDiem";
-import Style_Voucher from "./Style_Voucher";
-import StyleGiaoDich from "./Giaodich";
-import Style_History_Tranform from "./Style_History_Tranform";
-import StyleVoucherCGV from "./StyleVoucherCGV";
-import StyleGioHang from "./StyleGioHang";
-import StyleChonVoucher from "./StyleChonVoucher";
-import StylePhuongthucthanhtoan from "./StylePhuongthucthanhtoan";
+import Style_Giao_Dich_TCong from './Style_Giao_Dich_TCong';
+import Style_History_Tranform from './Style_History_Tranform';
+import Style_Chitiet_giaodich from './Style_Chitiet_giaodich';
+import Style_Voucher from './Style_Voucher';
 const AppStyle = {
     StyleFirst,
     StyleSecond,
     StyleScreenXacNhanSDT,
     StyleMain,
     StyleLichSu,
-    StyleLichSuDiem,
     Style_Voucher,
+    Style_Giao_Dich_TCong,
     Style_History_Tranform,
-    StyleGiaoDich,
-    StyleVoucherCGV,
-    StyleGioHang,
-    StyleChonVoucher,
-    StylePhuongthucthanhtoan 
+    Style_Chitiet_giaodich
 }
 export default AppStyle;
