bbacd10cd7b3bca42654033a041a86e7a4d6b011
Pham Jin <vanquy33338888@gmail.com>
2021-02-25 03:13:51 -0800

    Nam

--- index.js
+++ index.js
@@ -6,5 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Chitiet_giaodich from './src/components/Chitiet_giaodich';
-import Test from "./src/components/Test";
 AppRegistry.registerComponent(appName, () =>App);
