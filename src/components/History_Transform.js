de50cc041755ca3808bd6bec67f6d520e6c1340b
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2021-03-03 13:35:37 +0700

    Nam commit calendar

--- src/components/History_Transform.js
+++ src/components/History_Transform.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { View, Text, Image, Alert, TouchableOpacity, ScrollView, Modal } from 'react-native';
 import AppStyle from "../theme";
